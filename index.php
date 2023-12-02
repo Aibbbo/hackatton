fb8656d86f353b20b2d5b87ad03159dc656a87ab
Aibbbo <151394083+Aibbbo@users.noreply.github.com>
2023-12-02 10:56:26 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1534,6 +1534,47 @@
                 </div>
             </div>
         </div>
-        
+        <?php
+
+$servername = "localhost";
+
+$username = "root";
+
+$password = "";
+
+$dbname = "qwerty";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    
+die("Ошибка подключения к базе данных: " . 
+$conn->connect_error);
+}
+
+
+$sql = "SELECT * FROM Users";
+
+$result = $conn->query($sql);
+
+
+if ($result->num_rows > 0) {
+ 
+   while($row = $result->fetch_assoc()) {
+     
+   echo "<tr><td>".$row["uin"]."</td><td>".$row["full name"]."</td><td>".$row["subject"]."</td>
+<td>".$row["theme"]."</td><td>".$row["email"]."</td><td>".$row["phone number"]."</td><td>".$row["full name in english"]."</td><td>".$row["project"]."</td></tr>";
+    }
+} else {
+   
+ echo "Нет результатов";
+
+}
+
+$conn->close();
+
+?>
     </body>
 </html>
